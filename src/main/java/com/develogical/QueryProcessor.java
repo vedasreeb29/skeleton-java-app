a87558b2e80640364d742f53502132b95c93acbd
Bojanapally Veda Sree <vedasree.b29@gmail.com>
2020-06-24 13:15:17 +0530

    just a sample commit to test travis integration

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,7 +4,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.toLowerCase().contains("romeo and juliet")) {
-            return "William Shakespeare";
+            return "The William Shakespeare";
         }
         return "";
     }
